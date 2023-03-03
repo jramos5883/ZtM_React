eccb486ed73647d7c9bb67db330e3452e223d547
John Ramos <jramos5883@gmail.com>
2023-03-03 15:11:21 -0800

    Working on formating page, and starting to set up card components.

--- mainProj/react-capstone-proj/src/App.js
+++ mainProj/react-capstone-proj/src/App.js
@@ -1,4 +1,38 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneakers",
+    },
+    {
+      id: 1,
+      title: "Womans",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default App;
